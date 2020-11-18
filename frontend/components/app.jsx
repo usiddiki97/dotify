b07e7f012b68be518d2d0b6e4669d44adadd7eac
usiddiki97 <usiddiki97@gmail.com>
2020-11-18 16:15:53 -0500

    Finish frontend auth functionality

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container'
 import LoginFormContainer from './session_form/login_form_container'
 import SignupFormContainer from './session_form/signup_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div>
         <h1>Dotify</h1>
         <GreetingContainer />
 
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+        <Switch>
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch>
     </div>
 );
 
