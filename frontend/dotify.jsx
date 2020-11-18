891d6fbd82a48d61cf3f26a5de01a33751ae3a78
usiddiki97 <usiddiki97@gmail.com>
2020-11-18 06:51:39 -0500

    Create Store

--- frontend/dotify.jsx
+++ frontend/dotify.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
     ReactDOM.render(<h1>Hi, welcome to day 1 of suffering</h1>, root);
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch; 
 })
